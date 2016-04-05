610d45f2ff21ec69124ec791cdd53946633ce14f
卖小女孩的小火柴 <364810520@qq.com>
2016-04-05 21:29:39 +0800

    修复描述错误和增加gitignore

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,9 @@ defined('APP_PATH') or define('APP_PATH', BASE_PATH . '/app');
 //配置文件路径
 defined('CONFIG_PATH') or define('CONFIG_PATH', BASE_PATH . '/config');
 //静态文件路径
-defined('PUBLIC_PATH') or define('PUBLIC_PATH', BASE_PATH . '/public');
+defined('PUBLIC_PATH') or define('PUBLIC_PATH', 'public');
 //上传地址路径
-defined('UPLOAD_PATH') or define('UPLOAD_PATH', BASE_PATH . '/upload');
+defined('UPLOAD_PATH') or define('UPLOAD_PATH',  'upload');
 //缓存地址路径
 defined('CACHE_PATH') or define('CACHE_PATH', BASE_PATH . '/cache');
 
