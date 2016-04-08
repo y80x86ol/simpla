3e84cdde278a1fc43817c1170c57f3842283cd61
y80x86ol <364810520@qq.com>
2016-04-08 21:36:37 +0800

    增加example示例

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,44 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Simpla</title>
-		<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-		<style>
-			html, body {
-				height: 100%;
-			}
+    <head>
+        <meta charset="UTF-8">
+        <title>Simpla</title>
+        <style>
+            /* latin-ext */
+            @font-face {
+                font-family: 'Lato';
+                font-style: normal;
+                font-weight: 100;
+                src: local('Lato Hairline'), local('Lato-Hairline'), url(https://fonts.gstatic.com/s/lato/v11/eFRpvGLEW31oiexbYNx7Y_esZW2xOQ-xsNqO47m55DA.woff2) format('woff2');
+                unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
+            }
+            /* latin */
+            @font-face {
+                font-family: 'Lato';
+                font-style: normal;
+                font-weight: 100;
+                src: local('Lato Hairline'), local('Lato-Hairline'), url(https://fonts.gstatic.com/s/lato/v11/GtRkRNTnri0g82CjKnEB0Q.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+            }
+            html, body {
+                height: 100%;
+            }
 
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
 
-			.title {
-				font-size: 96px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Welcome To Simpla</div>
-			</div>
-		</div>
-	</body>
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Welcome To Simpla</div>
+            </div>
+        </div>
+    </body>
 </html>
