c842bd31c0e9f9d9bafb4b1a4c802a8c692bc774
y80x86ol <364810520@qq.com>
2016-04-20 23:04:46 +0800

    文件夹分布修改

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,12 +9,6 @@
  */
 //基础路径
 defined('BASE_PATH') or define('BASE_PATH', dirname(dirname(__FILE__)));
-//app应用路径
-defined('APP_PATH') or define('APP_PATH', BASE_PATH . '/app');
-//配置文件路径
-defined('CONFIG_PATH') or define('CONFIG_PATH', BASE_PATH . '/config');
-//缓存地址路径
-defined('CACHE_PATH') or define('CACHE_PATH', BASE_PATH . '/cache');
 
 //引入自动加载
 require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
